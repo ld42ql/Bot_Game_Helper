c65b466c0771b242b97fcdf7acaa03d5b2a51cbe
Vladimir baleev <ld42ql@hotmail.com>
2017-12-02 18:08:41 +0300

    Добавленно несколько операций

--- volshebkaHelper/Program.cs
+++ volshebkaHelper/Program.cs
@@ -3,13 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Threading;
 
 namespace volshebkaHelper
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            WebClient webClient = new WebClient();
+            WorkingBot user;
+
+            int update_id = 0;
+            string messageFromId = String.Empty;
+            string messageText = String.Empty;
+            string lineResult = String.Empty;
+
+            string errorMsg = String.Empty;
+
+            bool flag = true;
+
+            while (true)
+            {
+                string url = $"{UserSet.startURL}/getUpdates?offset={update_id + 1}";
+                string response = webClient.DownloadString(url);
+
+                var list = JObject.Parse(response)["result"].ToArray();
+
+                foreach (var message in list)
+                {
+                    update_id = Convert.ToInt32(message["update_id"]);
+                    try
+                    {
+                        messageFromId = message["message"]["from"]["id"].ToString();
+                        messageText = message["message"]["text"].ToString();
+
+                        if (flag)
+                        {
+                            webClient.DownloadString($"https://api.telegram.org/bot451203706:AAEXY_QgsHxwwxyqu3t92Lybv0RM2LkirlY/sendMessage?chat_id={messageFromId}&text={Operations.ErrorAnswer()}");
+                            flag = false;
+                        }
+                        user = new WorkingBot(messageFromId, messageText);
+
+                        lineResult = $"\n****************\n{update_id} {messageFromId} {messageText} {DateTime.Now}\n****************\n";
+
+                        Console.WriteLine($"{lineResult}");
+
+                        string str = user.AnswerBots;
+
+                        if (str != "")
+                        {
+                            webClient.DownloadString(str);
+                        }
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        string error = $"\n****************\n{e.ToString()}\n{DateTime.Now}\n****************\n";
+                        Console.WriteLine($"{error}");
+                        errorMsg += error;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
         }
     }
 }
---
+++ volshebkaHelper/WorkingBot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace volshebkaHelper
+{
+    class WorkingBot : UserSet
+    {
+        public WorkingBot(string messageFromId, string messageText) : base(messageFromId, messageText)
+        {
+        }
+
+        private string answerBots = String.Empty;
+        public string AnswerBots
+        {
+            get
+            {
+                SelectOperations();
+                return answerBots;
+            }
+        }
+
+        /// <summary>
+        /// Формируем ответ
+        /// </summary>
+        /// <param name="operations">Для какого метода</param>
+        string AnswerMessage(PossibleOperations operations)
+        {
+            string str = $"{startURL}/sendMessage?chat_id={MessageFromId}&text={operations()}";
+            return str;
+        }
+
+        /// <summary>
+        /// Выбор ответа
+        /// </summary>
+        void SelectOperations()
+        {
+            if (!Char.IsLetter(MessageText, 0))
+            {
+                switch (MessageText.Substring(1, MessageText.Length - 1))
+                {
+                    case "путешествие": answerBots = AnswerMessage(operations: Operations.EventRandom); break;
+                    case "кубик": answerBots = AnswerMessage(operations: Operations.ResultDice); break;
+                    default: answerBots = AnswerMessage(operations: Operations.ErrorAnswer); break;
+                }
+            }
+        }
+    }
+}
